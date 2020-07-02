02ee2e135b9251f32a30f30dd316be932d65399c
Salvatore Fasano <fasanosalvatore@hotmail.it>
2020-07-02 19:36:10 +0200

    Aggiornamento script

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# babel-script
+# Babel script shell
+
+Un semplice script per inizializzare babel con @babel/preset-env e @babel/plugin-proposal-class-properties, compatibile con Eslint.
--- script.sh
+++ script.sh
@@ -1,8 +1,21 @@
+LCYAN='\033[1;36m'
+YELLOW='\033[1;33m'
+GREEN='\033[1;32m'
+NC='\033[0m'
+
+echo
+echo -e "${LCYAN}Installo Babel ed i componenti aggiuntivi... ${NC}"
+echo
+
 npm i -D @babel/core @babel/preset-env @babel/plugin-proposal-class-properties babel-eslint
+
+echo
+echo -e "${YELLOW}Creo il tuo file .babelrc... ${NC}"
+
 > .babelrc
-echo '
-{
+echo '{
   "presets": ["@babel/preset-env"],
   "plugins": ["@babel/plugin-proposal-class-properties"]
-}
-' >> .babelrc
+}' >> .babelrc
+
+echo -e "${GREEN}Configurazione terminata! ${NC}"
